6aa7b6c9379521a024509d510d24fa1f7892f726
unknown <you@example.com>
2020-10-17 14:14:50 +0300

    hm2

--- homework3/index.js
+++ homework3/index.js
@@ -1,46 +1,20 @@
 
-// const friends = ["ivan", "petro", "danya", "polina", "vlad", "alex"];
-// for(let i = 0; i < friends.length; i+=1){
-//     console.log(`${i+1} - ${friends[i]}`);
-// }
 
 
 
 
+ const logItems = function (array) {
+     for(let i = 0; i < array.length; i +=1){
+         console.log(`${i+1} - ${array[i]}`);
+     }
+
+   };
+
+
+ logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+ logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
 
 
 
-
-
-// const logItems = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
-// let i = 0;
-// for(const banana of logItems){
-//     //console.log(friend);
-//     console.log(`${i+=1} - ${banana}`);;
-// }
-
-
-
-
-
-// const logItems = function (array) {
-//     for(let i = 0; i < array.length; i +=1){
-//         console.log(`${i+1} - ${array[i]}`);
-//     }
-    
-//   };
-
-
-// logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
-
-// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
-
-
-
-
-
-let i = 3;
-while(i){
-  console.log(i -=1);
-}
