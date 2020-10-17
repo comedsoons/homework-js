81470c40018a0934f61f4b9d11dd70e5ff76d0e9
unknown <you@example.com>
2020-10-17 14:17:22 +0300

    hm3

--- homework3/index.js
+++ homework3/index.js
@@ -3,8 +3,8 @@
 
 
 
- const logItems = function (array) {
-     for(let i = 0; i < array.length; i +=1){
+ const logItems = function ( array ) {
+     for( let i = 0; i < array.length; i +=1 ){
          console.log(`${i+1} - ${array[i]}`);
      }
 
