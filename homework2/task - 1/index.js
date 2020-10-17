68b6474d62585e1542b76b399f73644fd6d549a7
unknown <you@example.com>
2020-10-17 12:28:51 +0300

    third homework

--- homework2/task - 1/index.js
+++
@@ -1,7 +0,0 @@
-const friends = ['ivan', 'petro', 'danya', 'sasha', 'polina'];
-for (const friend of friends){
-    if(friend === 'danya'){
-        console.log('знайдено найкращого друга');
-        break;
-    }
-}
---
+++ homework3/index.js
@@ -0,0 +1,42 @@
+
+// const friends = ["ivan", "petro", "danya", "polina", "vlad", "alex"];
+// for(let i = 0; i < friends.length; i+=1){
+//     console.log(`${i+1} - ${friends[i]}`);
+// }
+
+
+
+
+
+
+
+
+
+
+// const logItems = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+// let i = 0;
+// for(const banana of logItems){
+//     //console.log(friend);
+//     console.log(`${i+=1} - ${banana}`);;
+// }
+
+
+
+
+
+const logItems = function (array) {
+    for(let i = 0; i < array.length; i +=1){
+        console.log(`${i+1} - ${array[i]}`);
+    }
+    
+  };
+
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+
+
+
+
