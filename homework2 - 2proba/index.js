600c60c70a216f8ac6956119b696a0f60954441d
unknown <you@example.com>
2020-10-17 14:12:01 +0300

    hm2

---
+++ homework2 - 2proba/index.js
@@ -0,0 +1,10 @@
+const friends = ["oleg", "marek", "artem", "vova", "ivan"];
+
+for (const friend of friends) {
+    if (friend === "oleg") {
+        console.log("Найкращого друга знайдено!");
+        break;
+    } else {
+        console.log("Найкращого друга не знайдено!");
+    }
+}
