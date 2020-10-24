daeca5fddbeb10ef35154a320cbd77367017efbf
unknown <you@example.com>
2020-10-24 12:07:48 +0300

    hm4

--- homework4/index.js
+++ homework4/index.js
@@ -1,1 +1,33 @@
-xz
+const calculateEngravingPrice = function (message, pricePerWord) {
+    let numberOfWords = 0;
+    for (let i=0; i < message.length; i +=1){
+        if(message[i]=== ' '){
+            numberOfWords += 1;
+      }
+    }
+    numberOfWords += 1;
+    return numberOfWords*pricePerWord;
+  };
+
+
+  console.log(
+    calculateEngravingPrice(
+      'Proin sociis natoque et magnis parturient montes mus',
+      10,
+    ),
+  );
+
+  console.log(
+    calculateEngravingPrice(
+      'Proin sociis natoque et magnis parturient montes mus',
+      20,
+    ),
+  );
+
+  console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+  );
+
+  console.log(
+    calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+  );
